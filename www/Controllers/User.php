f71ec5dc052a13c70c72c701d1d11a2950609c55
Paul Becue <paulbecue3105@gmail.com>
2023-06-29 08:28:52 +0200

    Début routing
    Signed-off-by: Paul Becue <paulbecue3105@gmail.com>

--- www/Controllers/User.php
+++ www/Controllers/User.php
@@ -1,20 +1,56 @@
 <?php
 namespace App\Controllers;
+
 use App\Core\View;
 
-class User{
-    public function index(){
-
+class User
+{
+    public function index()
+    {
         $pseudo = "Prof";
         $view = new View("Main/index", "front");
         $view->assign("pseudo", $pseudo);
+        
+        // Vérifier le format de retour demandé
+        $format = $this->getRequestFormat();
+        
+        // Adapter la réponse en fonction du format demandé
+        if ($format === 'json') {
+            header('Content-Type: application/json');
+            echo json_encode(['pseudo' => $pseudo]);
+        }else {
+            // Afficher la vue par défaut pour les autres formats
+            include "Main/index.view.php";
+        }
     }
 
-    public function contact(){
-        $view = new View("Main/contact", "front");
-    }
+    public function contact()
+{
+    // Vérifier le format de retour demandé
+    $format = $this->getRequestFormat();
 
-    public function dashboard(){
-        echo "Mon tableau de bord";
+    // Adapter la réponse en fonction du format demandé
+    if ($format === 'json') {
+        header('Content-Type: application/json');
+        echo json_encode(['message' => 'Contact form submitted']);
+    }else {
+        // Afficher la vue par défaut pour les autres formats
+        include "Main/contact.view.php";
+    }
+}
+
+    private function getRequestFormat()
+    {
+        // Logique pour récupérer le format de retour demandé
+        // Cela peut être basé sur des en-têtes HTTP ou des paramètres de requête
+        // Dans cet exemple, je suppose que le format est spécifié via un paramètre "format" dans l'URL
+    
+        $format = 'html'; // Format par défaut
+    
+        if (isset($_GET['format'])) {
+            $format = strtolower($_GET['format']);
+        }
+    
+        return $format;
     }
 }
--- www/index.php
+++ www/index.php
@@ -3,86 +3,72 @@ namespace App;
 //Contrainte : utilisation des Namespace
 
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 spl_autoload_register(function ($class) {
-    //Core/View.php
-    $class = str_replace("App\\","", $class);
-    $class = str_replace("\\","/", $class).".php";
-    if(file_exists($class)){
+    $class = str_replace("App\\", "", $class);
+    $class = str_replace("\\", "/", $class) . ".php";
+    if (file_exists($class)) {
         include $class;
     }
 });
 
+// Récupérer l'URI de l'URL
+$uriExploded = explode("?", $_SERVER["REQUEST_URI"]);
+$uri = rtrim(strtolower(trim($uriExploded[0])), "/");
+// Dans le cas où nous sommes à la racine, $uri sera vide, donc on remet "/"
+$uri = (empty($uri)) ? "/" : $uri;
 
-
-//Récupérer dans l'url l'uri /login ou /user/toto
-//Nettoyer la donnée
-//S'il y a des paramètres dans l'url il faut les enlever :
-$uriExploded = explode("?",$_SERVER["REQUEST_URI"]);
-$uri = rtrim(strtolower(trim($uriExploded[0])),"/");
-//Dans le cas ou nous sommes à la racine $uri sera vide du coup je remets /
-$uri = (empty($uri))?"/":$uri;
-
-//Créer un fichier yaml contenant le route du type :
-// /login:
-//      controller: Security
-//      action: login
-//Le fichier est dans www et porte le nom de routes.yml
-
-//En fonction de l'uri récupérer le controller et l'action
-//Effectuer toutes les vérifications que vous estimez nécessaires
-//En cas d'erreur effectuer un simple Die avec un message explicite
-//Dans le cas ou tout est bon créer une instance du controller
-//et appeler la methode action
-
-//Exemple :
-// $controller = new Security();
-// $controller->login();
-
-if(!file_exists("routes.yml")) {
+// Charger les routes à partir du fichier YAML
+if (!file_exists("routes.yml")) {
     die("Le fichier de routing n'existe pas");
 }
 
 $routes = \yaml_parse_file("routes.yml");
 
-//Page 404
-if(empty($routes[$uri])) {
+// Vérifier si la route demandée existe
+if (empty($routes[$uri])) {
+    http_response_code(404);
     die("Page 404");
 }
 
-if(empty($routes[$uri]["controller"]) || empty($routes[$uri]["action"])) {
-    die("Absence de controller ou d'action dans le ficher de routing pour la route ".$uri);
+// Récupérer le contrôleur et l'action de la route
+if (empty($routes[$uri]["controller"]) || empty($routes[$uri]["action"])) {
+    http_response_code(500);
+    die("Absence de controller ou d'action dans le fichier de routing pour la route " . $uri);
 }
 
 $controller = $routes[$uri]["controller"];
 $action = $routes[$uri]["action"];
+$format = $routes[$uri]["format"]??"html";
 
-//Vérification de l'existance de la classe
-if(!file_exists("Controllers/".$controller.".php")){
-    die("Le fichier Controllers/".$controller.".php n'existe pas");
+// Vérification de l'existence du fichier de contrôleur
+$controllerFilePath = "Controllers/" . $controller . ".php";
+if (!file_exists($controllerFilePath)) {
+    http_response_code(500);
+    die("Le fichier " . $controllerFilePath . " n'existe pas");
 }
 
-include "Controllers/".$controller.".php";
+include $controllerFilePath;
 
-//Le fichier existe mais est-ce qu'il possède la bonne classe
-//bien penser à ajouter le namespace \App\Controllers\Security
-$controller = "\\App\\Controllers\\".$controller;
-if(!class_exists($controller)){
-    die("La class ".$controller." n'existe pas");
+// Vérifier l'existence de la classe du contrôleur
+$controller = "\\App\\Controllers\\" . $controller;
+if (!class_exists($controller)) {
+    http_response_code(500);
+    die("La classe " . $controller . " n'existe pas");
 }
+header('Content-Type: '.$format);
 
 $objet = new $controller();
 
-//Est-ce que l'objet contient bien la methode
-if(!method_exists($objet, $action)){
-    die("L'action ".$action." n'existe pas");
+// Vérifier si l'action existe dans le contrôleur
+if (!method_exists($objet, $action)) {
+    http_response_code(500);
+    die("L'action " . $action . " n'existe pas");
 }
-
-$objet->$action();
-
-
-
-
-
+$response = $objet->$action();
 
 
 
