3d883c36833e6062bc0cb7e32c99b8e9a7ae65cd
Estelle <estelle272001@gmail.com>
2023-07-09 23:34:31 +0200

    Add page Builder

--- www/Controllers/Page.php
+++ www/Controllers/Page.php
@@ -5,14 +5,15 @@ namespace App\Controllers;
 use App\Core\Sql;
 use App\Core\View;
 use App\Models\Page as Build;
-
+use PHPageBuilder\PHPageBuilder;
+use PHPageBuilder\Modules\GrapesJS\PageBuilder;
 class Page extends Sql
 {
     public function updateTheme(): void
     {
         $view = new View("Dash/pageBuilder","builder");
     }
-    public function newPage(): void
+    public function createPage(): void
     {
         $view = new View("Dash/pageBuilder","builder");
         if($_POST){
@@ -28,6 +29,24 @@ class Page extends Sql
                 $pageBuild->save();
             }
         }
+
+    }
+
+    public function newPage(){
+        $installationFolder = __DIR__ .'/../Views/PageBuilder';
+        require_once $installationFolder . '/src/Core/helpers.php';
+        spl_autoload_register('phpb_autoload');
+
+        $config = require $installationFolder . '/config/config.example.php';
+        $builder = new PHPageBuilder($config);
+        $builder->handleRequest();
+
+        // Générer la page avec PHPageBuilder
+        $html = $builder->generate();
+
+        // Afficher la page
+        $view = new View("Dash/pageBuilder", "builder");
+        $view->assign("html", $html);
 
     }
 }
