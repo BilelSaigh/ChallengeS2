2040d2f22dd4c66f6d04ff3c5057745d4db46b86
BilelSaigh <bilelsaigh94@gmail.com>
2023-07-18 18:33:37 +0200

    suppression submodule

---
+++ www/Controllers/Installer.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controllers;
+
+class Installer
+{
+    public function index()
+    {
+        $filePath = '/ChallengeJs/index.html';
+        if (file_exists($filePath)) {
+            header('Content-Type: text/html');
+            readfile($filePath);
+        } else {
+            // Gérer l'erreur si le fichier n'est pas trouvé
+            throw new \Exception("Le fichier index.html n'a pas été trouvé");
+        }
+    }
+}
