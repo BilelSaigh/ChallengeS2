6a8e5969c80e849d564028e4948b53a128f9764a
Bilel Saigh <bilelsaigh94@gmail.com>
2023-07-20 18:22:20 +0200

    fix router

--- www/Core/Router.php
+++ www/Core/Router.php
@@ -1,4 +1,6 @@
 <?php
+
+/**
 namespace App\Core;
 //Contrainte : utilisation des Namespace
 
@@ -21,44 +23,38 @@ class Router
             throw new \Exception("Page non trouvée", 404);
         }
 
-
-                // Recherche de la route correspondante avec un slug dynamique
-        $matchedRoute = null;
-        $matchedParams = [];
-
-        foreach ($routes as $route => $config) {
-        if (strpos($route, '{slug}') !== false) {
-            // Si la route contient "{slug}", il s'agit d'une route avec un slug
-            $pattern = str_replace('{slug}', '([^/]+)', $route);
-            $regex = '#^' . $pattern . '$#';
-
-            if (preg_match($regex, $uri, $matches)) {
-                $matchedRoute = $route;
-                $matchedParams = [];
-
-                // Récupérer les valeurs des paramètres
-                for ($i = 1; $i < count($matches); $i++) {
-                    $matchedParams[] = $matches[$i];
-                }
-                break;
-            }
-        } else {
-                // Si la route est une correspondance exacte
-                if ($uri === $route) {
-                    $matchedRoute = $route;
-                    break;
-                }
-            }
-        }
-
-        if ($matchedRoute === null) {
-        // Page 404
-            http_response_code(404);
-            $view = new View("Error/404", "error");
-        }
-
-
         $route = $this->routes[$uri];
+
+
+           // Recherche de la route correspondante avec un slug dynamique
+           $matchedRoute = null;
+           $matchedParams = [];
+
+           foreach ($routes as $route => $config) {
+               if (strpos($route, '{slug}') !== false) {
+                   // Si la route contient "{slug}", il s'agit d'une route avec un slug
+                   $pattern = str_replace('{slug}', '([^/]+)', $route);
+                   $regex = '#^' . $pattern . '$#';
+
+                   if (preg_match($regex, $uri, $matches)) {
+                       $matchedRoute = $route;
+                       $matchedParams = [];
+
+                       // Récupérer les valeurs des paramètres
+                       for ($i = 1; $i < count($matches); $i++) {
+                           $matchedParams[] = $matches[$i];
+                       }
+                       break;
+                   }
+               } else {
+                   // Si la route est une correspondance exacte
+                   if ($uri === $route) {
+                       $matchedRoute = $route;
+                       break;
+                   }
+               }
+           }
+
 
         if (empty($route["controller"]) || empty($route["action"])) {
             throw new \Exception("Absence de controller ou d'action dans le ficher de routing pour la route ".$uri);
@@ -74,7 +70,7 @@ class Router
             throw new \Exception("La class ".$controller." n'existe pas", 500);
         }
 
-        
+
 
         $controllerInstance = new $controller();
 
@@ -89,9 +85,67 @@ class Router
         // echo "Action : " . $action . PHP_EOL;
 
         $controllerInstance->$action();
-        
+
 
     }
 }
+**/
+
+
+namespace App\Core;
+//Contrainte : utilisation des Namespace
+
+
+class Router
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $this->routes = yaml_parse_file("routes.yml");
+    }
+
+    public function routeRequest()
+    {
+        $uriExploded = explode("?", $_SERVER["REQUEST_URI"]);
+        $uri = rtrim(strtolower(trim($uriExploded[0])), "/");
+        $uri = (empty($uri)) ? "/" : $uri;
+
+        if (empty($this->routes[$uri])) {
+            throw new \Exception("Page non trouvée", 404);
+        }
+
+
+        $route = $this->routes[$uri];
+
+        if (empty($route["controller"]) || empty($route["action"])) {
+            throw new \Exception("Absence de controller ou d'action dans le ficher de routing pour la route " . $uri);
+        }
+
+        $controller = "\\App\\Controllers\\" . $route["controller"];
+        $action = $route["action"];
+
+        if (!class_exists($controller)) {
+            throw new \Exception("La class " . $controller . " n'existe pas", 500);
+        }
+
+
+        $controllerInstance = new $controller();
+
+        if (!method_exists($controllerInstance, $action)) {
+            throw new \Exception("L'action " . $action . " n'existe pas", 500);
+        }
+
+
+        // echo "Route trouvée : " . $uri . PHP_EOL;
+        // echo "Contrôleur : " . $controller . PHP_EOL;
+        // echo "Action : " . $action . PHP_EOL;
+
+        $controllerInstance->$action();
+
+
+    }
+}
+
 
 ?>
--- www/index.php
+++ www/index.php
@@ -7,7 +7,8 @@ use App\Core\View;
 
 session_start();
 
-// Contrainte : utilisation des Namespace
+// Constraint : utilisation des Namespace
+
 spl_autoload_register(function ($class) {
     // Core/View.php
     $class = str_replace("App\\","", $class);
